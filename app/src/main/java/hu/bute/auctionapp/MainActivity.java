faa9c20b33137ee3f408765b8b038be1cd6cabe9
varsi94 <varsi.marci@gmail.com>
2014-12-04 12:37:26 +0100

    Próba Parse

--- app/src/main/java/hu/bute/auctionapp/MainActivity.java
+++ app/src/main/java/hu/bute/auctionapp/MainActivity.java
@@ -2,8 +2,14 @@ package hu.bute.auctionapp;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.SaveCallback;
 
 
 public class MainActivity extends Activity {
@@ -12,28 +18,26 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        Button b = (Button) findViewById(R.id.testBtn);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+                ParseObject obj = new ParseObject("Fileclass");
+                ParseFile f = new ParseFile("valami.txt", new byte[]{1,2,3,4,5,6,7,8,9,0});
+                obj.put("enfajlom", f);
+                obj.put("nev", "kula");
+                f.saveInBackground();
+                obj.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if (e == null) {
+                            Toast.makeText(MainActivity.this, "Uploaded", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }
+        });
     }
 }
