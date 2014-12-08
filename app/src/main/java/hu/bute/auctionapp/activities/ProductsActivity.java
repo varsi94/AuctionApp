6b4509bed088d4e5889da02b43067d948d568dce
nagyfaszu <sipkabe@gmail.com>
2014-12-08 15:41:52 +0100

    strings fix?

--- app/src/main/java/hu/bute/auctionapp/activities/ProductsActivity.java
+++ app/src/main/java/hu/bute/auctionapp/activities/ProductsActivity.java
@@ -143,11 +143,11 @@ public class ProductsActivity extends Activity implements ActionBar.TabListener 
             Locale l = Locale.getDefault();
             switch (position) {
                 case 0:
-                    return getString(R.string.title_section1).toUpperCase(l);
+                    return getString(R.string.title_section).toUpperCase(l);
                 case 1:
-                    return getString(R.string.title_section2).toUpperCase(l);
+                    return getString(R.string.title_section).toUpperCase(l);
                 case 2:
-                    return getString(R.string.title_section3).toUpperCase(l);
+                    return getString(R.string.title_section).toUpperCase(l);
             }
             return null;
         }
