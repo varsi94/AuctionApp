6382be2e3a7a7c9078323965ded1eebadc89274b
varsi94 <varsi.marci@gmail.com>
2014-12-10 15:36:55 +0100

    Storehoz lehet új képet is feltölteni

--- app/src/main/java/hu/bute/auctionapp/activities/UploadActivity.java
+++ app/src/main/java/hu/bute/auctionapp/activities/UploadActivity.java
@@ -7,8 +7,6 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -215,27 +213,5 @@ public class UploadActivity extends Activity {
                 locationInfo = info;
             }
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_upload, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/hu/bute/auctionapp/activities/UploadStoreActivity.java
+++ app/src/main/java/hu/bute/auctionapp/activities/UploadStoreActivity.java
@@ -7,6 +7,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -16,6 +17,8 @@ import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import java.io.File;
+
 import hu.bute.auctionapp.AuctionApplication;
 import hu.bute.auctionapp.R;
 import hu.bute.auctionapp.data.StoreData;
@@ -24,10 +27,14 @@ import hu.bute.auctionapp.parsewrapper.CloudHandler;
 
 public class UploadStoreActivity extends Activity {
     private static final int PICK_IMAGE = 1562;
+    private static final int GET_IMAGE_FROM_CAMERA = 15156;
+    private static final String IMAGEPATH = Environment.getExternalStorageDirectory().getAbsolutePath() + "/tmp_image.jpg";
+    ;
     private ImageButton pickImageBtn;
     private ImageButton okBtn;
     private ImageButton cancelBtn;
     private ImageButton deleteImageBtn;
+    private ImageButton createImageBtn;
     private View.OnClickListener btnClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
@@ -40,17 +47,22 @@ public class UploadStoreActivity extends Activity {
                 finish();
             } else if (view.equals(deleteImageBtn)) {
                 deleteImage();
+            } else if (view.equals(createImageBtn)) {
+                pickImageFromCamera();
             }
         }
     };
+
     private Spinner storeTypeSpinner;
     private EditText storeNameET;
     private ImageView previewImage;
     private String pictureFileName;
+    private boolean hasPhoto;
 
     private void deleteImage() {
         previewImage.setImageResource(R.drawable.nophoto);
         pictureFileName = null;
+        hasPhoto = false;
     }
 
     @Override
@@ -74,13 +86,34 @@ public class UploadStoreActivity extends Activity {
             int imgWidth = opt.outWidth;
 
             int realWidth = 128;
-            int scaleFactor = Math.round((float)imgWidth / (float)realWidth);
+            int scaleFactor = Math.round((float) imgWidth / (float) realWidth);
             opt.inSampleSize = scaleFactor;
             opt.inJustDecodeBounds = false;
 
-            Bitmap img = BitmapFactory.decodeFile(filePath,opt);
+            Bitmap img = BitmapFactory.decodeFile(filePath, opt);
             previewImage.setImageBitmap(img);
             pictureFileName = filePath;
+            hasPhoto = true;
+        } else if (requestCode == GET_IMAGE_FROM_CAMERA && resultCode == RESULT_OK) {
+            try {
+                BitmapFactory.Options opt = new BitmapFactory.Options();
+                opt.inJustDecodeBounds = true;
+                BitmapFactory.decodeFile(IMAGEPATH, opt);
+                int imgWidth = opt.outWidth;
+
+                int realWidth = previewImage.getMeasuredWidth();
+                int scaleFactor = Math.round((float) imgWidth / (float) realWidth);
+                opt.inSampleSize = scaleFactor;
+                opt.inJustDecodeBounds = false;
+
+                Bitmap img = BitmapFactory.decodeFile(IMAGEPATH, opt);
+
+                previewImage.setImageBitmap(img);
+                pictureFileName = IMAGEPATH;
+                hasPhoto = true;
+            } catch (Throwable t) {
+                t.printStackTrace();
+            }
         }
     }
 
@@ -93,7 +126,9 @@ public class UploadStoreActivity extends Activity {
         Toast.makeText(this, R.string.uploading, Toast.LENGTH_LONG).show();
         String storeName = storeNameET.getText().toString();
         StoreData data = new StoreData(storeName, 0, (String) storeTypeSpinner.getSelectedItem());
-        data.setPictureFileName(pictureFileName);
+        if (hasPhoto) {
+            data.setPictureFileName(pictureFileName);
+        }
         AuctionApplication app = (AuctionApplication) getApplication();
         app.cloud.saveStore(data, new CloudHandler.ResultCallback() {
             @Override
@@ -121,6 +156,8 @@ public class UploadStoreActivity extends Activity {
         cancelBtn.setOnClickListener(btnClickListener);
         deleteImageBtn = (ImageButton) findViewById(R.id.deleteImageBtn);
         deleteImageBtn.setOnClickListener(btnClickListener);
+        createImageBtn = (ImageButton) findViewById(R.id.createImageBtn);
+        createImageBtn.setOnClickListener(btnClickListener);
 
         storeNameET = (EditText) findViewById(R.id.storeNameUploadET);
         previewImage = (ImageView) findViewById(R.id.imagePreview);
@@ -128,10 +165,21 @@ public class UploadStoreActivity extends Activity {
         ArrayAdapter<String> adapter =
                 new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, StoreTypes.getValues(this));
         storeTypeSpinner.setAdapter(adapter);
+
+        hasPhoto = true;
     }
 
     private void pickImage() {
         Intent i = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         startActivityForResult(i, PICK_IMAGE);
+    }
+
+    private void pickImageFromCamera() {
+        File imageFile = new File(IMAGEPATH);
+        Uri imageFileUri = Uri.fromFile(imageFile);
+        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT,
+                imageFileUri);
+        startActivityForResult(cameraIntent, GET_IMAGE_FROM_CAMERA);
     }
 }
